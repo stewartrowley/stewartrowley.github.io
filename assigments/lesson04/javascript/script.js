69aca2701d45de530beb24e3a7379c2fcc54ebe8
Row84M <stewy.rowley@gmail.com>
2021-10-16 18:17:36 -0600

    update

--- assigments/lesson04/javascript/script.js
+++ assigments/lesson04/javascript/script.js
@@ -14,3 +14,4 @@ directionbutton.addEventListener('click', () => {mainnav.classList.toggle('respo
 
 // To solve the mid resizing issue with responsive class on
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
---
+++ assigments/lesson05/javascript/rexburgscript.js
@@ -0,0 +1,27 @@
+let today = new Date();
+let day = today.getDay();
+
+if (day == 5) {
+    document.querySelector('#banner').style.display = 'block';
+  }
+
+
+window.onload =(event) => {
+
+    let update = document.lastModified
+    document.getElementById('timeDate').textContent = update;
+
+}
+
+
+
+const directionbutton = document.querySelector('.direction');
+const mainnav = document.querySelector('.navigation')
+
+directionbutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+
+  
