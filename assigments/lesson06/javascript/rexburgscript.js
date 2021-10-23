ab66cf7bb5fa3edfb464d0d2d19a0111868b392e
Row84M <stewy.rowley@gmail.com>
2021-10-23 12:21:51 -0600

    update

--- assigments/lesson06/javascript/rexburgscript.js
+++ assigments/lesson06/javascript/rexburgscript.js
@@ -23,5 +23,12 @@ directionbutton.addEventListener('click', () => {mainnav.classList.toggle('respo
 // To solve the mid resizing issue with responsive class on
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
 
+WebFont.load({
+  google: {
+    families: [ 'Roboto Condensed', 'sans-serif']
+  }
+});
+
+
 
   
